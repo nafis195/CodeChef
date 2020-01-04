e6b2626b4c5e5af059cec9813d1f17660d373b96
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2020-01-03 20:26:30 -0800

    Add files via upload

--- Breaking Bricks/Source.cpp
+++ Breaking Bricks/Source.cpp
@@ -1,1 +1,25 @@
+// Bismillahir Rahmanir Rahim
+// Nafis Chowdhury
+// Codechef- Breaking Bricks
+// Problem Statement - https://www.codechef.com/JAN20B/problems/BRKBKS
+// Main.cpp
 
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int testCases;
+	float sum, s, w1, w2, w3;
+
+	cin >> testCases;
+	for (int i = 0; i < testCases; i++)
+	{
+		cin >> s >> w1 >> w2 >> w3;
+		sum = (w1 + w2 + w3) / s;
+		cout << ceil(sum) << endl;
+	}
+
+	return 0;
+}
